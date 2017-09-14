1fbbc3b7bdb26c5a5a3e52cb0af7ffa33033e0a5
nivance <limingjian110@163.com>
2017-09-14 15:56:50 +0800

    修改文件分隔

--- other/calcDuration.sh
+++ other/calcDuration.sh
@@ -3,13 +3,11 @@ if [[ $# != 1 ]]; then
     echo "usage: $0 dir"
     exit -1
 fi
-SQL_FILE=./$1/update_audio.sql
->$SQL_FILE
 for file in `ls $1`
 do 
 	if [[ $file =~ .*\.etcb ]]; then
 		#echo $file
-		ns=(${file//./})
+		ns=(${file//\./ })
 		name=${ns[0]}
 		echo $name
 		sum=0
@@ -22,10 +20,8 @@ do
 				minute=${lengthTime:1:2}
 				second=${lengthTime:4:2}
 				sum=`expr $sum + 60 \* $minute + $second`
-				#echo "${lengthTime:1:5}----$minute----$second---"
 			fi
 		done
 		echo "$file's length : $sum"
-		echo "update t_audio set DURATION = $sum where NAME = '$name' AND STATUS = 1 AND AUTHENTICATE = 1;" >> $SQL_FILE
 	fi
 done
